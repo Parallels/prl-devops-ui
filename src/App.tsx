cc0c003fb6f07deb0d1b289c16c85c62121fe81c
cjlapao <cjlapao@gmail.com>
2026-03-25 13:53:25 +0000

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import "./App.scss";
-import { AppRouter } from "./router/AppRouter";
-import { ConfigProvider } from "./contexts/ConfigContext";
-import { SessionProvider } from "./contexts/SessionContext";
-import { WebSocketProvider } from "./contexts/WebSocketContext";
-import { EventsHubProvider } from "./contexts/EventsHubContext";
-import { SystemStatsProvider } from "./contexts/SystemStatsContext";
-import { ThemeProvider } from "./contexts/ThemeContext";
-import { LockedHostProvider } from "./contexts/LockedHostContext";
-import { IconProvider, BottomSheetProvider } from "@prl/ui-kit";
+import './App.scss';
+import { AppRouter } from './router/AppRouter';
+import { ConfigProvider } from './contexts/ConfigContext';
+import { SessionProvider } from './contexts/SessionContext';
+import { WebSocketProvider } from './contexts/WebSocketContext';
+import { EventsHubProvider } from './contexts/EventsHubContext';
+import { SystemStatsProvider } from './contexts/SystemStatsContext';
+import { ThemeProvider } from './contexts/ThemeContext';
+import { LockedHostProvider } from './contexts/LockedHostContext';
+import { IconProvider, BottomSheetProvider } from '@prl/ui-kit';
 import { renderIcon } from '@prl/ui-kit';
 
 function App() {
@@ -15,19 +15,19 @@ function App() {
     <ThemeProvider>
       <ConfigProvider>
         <LockedHostProvider>
-        <SessionProvider>
-          <WebSocketProvider>
-            <EventsHubProvider>
-              <SystemStatsProvider>
-                <IconProvider renderIcon={renderIcon}>
-                  <BottomSheetProvider>
-                    <AppRouter />
-                  </BottomSheetProvider>
-                </IconProvider>
-              </SystemStatsProvider>
-            </EventsHubProvider>
-          </WebSocketProvider>
-        </SessionProvider>
+          <SessionProvider>
+            <WebSocketProvider>
+              <EventsHubProvider>
+                <SystemStatsProvider>
+                  <IconProvider renderIcon={renderIcon}>
+                    <BottomSheetProvider>
+                      <AppRouter />
+                    </BottomSheetProvider>
+                  </IconProvider>
+                </SystemStatsProvider>
+              </EventsHubProvider>
+            </WebSocketProvider>
+          </SessionProvider>
         </LockedHostProvider>
       </ConfigProvider>
     </ThemeProvider>
